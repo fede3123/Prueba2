fa8cbcb9705fa61d8eaaba5831b5d5c2b37ddb89
Federico Rosero <federik4312@gmail.com>
2022-07-02 11:33:44 -0500

    create endpoints with db without vuejs

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uuid
 
 app = FastAPI()
 
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+
+def createDb(quantity):
+    arr = [(uuid.uuid4(), x) for x in range(quantity)]
+    result = {}
+    for key, value in arr:
+        result[key] = value
+
+    return result
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return templates.TemplateResponse('base.html', {})
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get('/request')
+async def request():
+    db = createDb(1000000)
+    return templates.TemplateResponse('response.html', {'db': db})
